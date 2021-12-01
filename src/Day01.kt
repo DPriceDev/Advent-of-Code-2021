d271132e9a15035d9996ca91e9ccbdb28fa19b22
David Price <David.Price@experian.com>
2021-12-01 08:01:50 +0000

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,14 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>) = input
+        .map { it.toInt() }
+        .windowed(2) { it.first() < it.last() }
+        .sumOf { if(it) 1L else 0L }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun part2(input: List<String>) = input
+        .map { it.toInt() }
+        .windowed(3) { it.sum() }
+        .windowed(2) { it.first() < it.last() }
+        .sumOf { if(it) 1L else 0L }
 
     val input = readInput("Day01")
     println(part1(input))
