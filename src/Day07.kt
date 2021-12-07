2e329816d285aa6726d53e0d7c37a2b14c09e6cd
David Price <David.Price@experian.com>
2021-12-07 19:54:58 +0000

    day 07

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,14 +1,59 @@
+import kotlin.math.abs
 
 fun main() {
 
     // Part 1
-    fun part1(input: List<String>) = input.size
+    fun part1(input: String) : Int {
+        val positions = input
+            .split(',')
+            .map { it.toInt() }
+
+        val max = positions.maxOf { it }
+        val min = positions.minOf { it }
+
+        val distanceMaps = (min..max).mapIndexed { index, target ->
+            target to positions.map { position ->
+                abs(position - target)
+            }
+        }
+
+        val distances = distanceMaps.map { it.first to it.second.sum() }
+        val minimumDistance = distances.reduce { acc, i ->
+            if(acc.second < i.second) acc else i
+        }
+
+        return minimumDistance.second
+    }
+
 
     // Part 2
-    fun part2(input: List<String>) = input.size
+    fun part2(input: String) : Int {
+        val positions = input
+            .split(',')
+            .map { it.toInt() }
+
+        val max = positions.maxOf { it }
+        val min = positions.minOf { it }
+
+        val distanceMaps = (min..max).map { target ->
+            target to positions.map { position -> abs(position - target) }
+        }
+
+        val distances = distanceMaps.map { (target, positions) ->
+            target to positions.sumOf { position ->
+                position * (position + 1) / 2
+            }
+        }
+
+        val minimumDistance = distances.reduce { acc, i ->
+            if(acc.second < i.second) acc else i
+        }
+
+        return minimumDistance.second
+    }
 
     // Output
-    val input = readInput("Day07")
+    val input = readInput("Day07").first()
     println(part1(input))
     println(part2(input))
 }
